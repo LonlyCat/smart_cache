9317c42298efdf62befd7b5fb6ded2a09cd9e4db
sen <lbs19312@live.com>
2025-04-01 10:23:03 +0800

    [ADD] CacheableLoader.load 允许控制返回缓存数据后是否继续请求以刷新缓存

--- lib/cacheable_loader.dart
+++ lib/cacheable_loader.dart
@@ -20,8 +20,8 @@ class CacheableLoadResult<T> {
   /// 从缓存加载成功的构造函数
   CacheableLoadResult.fromCache(this.data)
       : status = data != null
-          ? CacheableLoadStatus.fromCache
-          : CacheableLoadStatus.noneCache,
+            ? CacheableLoadStatus.fromCache
+            : CacheableLoadStatus.noneCache,
         error = null;
 
   /// 从加载器加载成功的构造函数
@@ -113,6 +113,7 @@ class CacheableLoader {
   /// [cacheOnly] 是否只从缓存加载，默认为false
   /// [loaderOnly] 是否只从加载器加载，默认为false
   /// [flushWhenSuccess] 是否在加载成功后刷新缓存，默认为 false
+  /// [refreshAfterCache] 是否在返回缓存后刷新缓存，默认为 false
   /// [cacheManager] 缓存管理器，不指定时使用标准缓存管理器
   /// [loaderValidator] 加载数据验证器，用于处理是否应该缓存加载器返回的数据
   ///
@@ -122,6 +123,7 @@ class CacheableLoader {
     bool cacheOnly = false,
     bool loaderOnly = false,
     bool flushWhenSuccess = false,
+    bool refreshAfterCache = false,
     SmartCacheManager? cacheManager,
     required Future<T?> Function() loader,
     bool Function(T? data)? loaderValidator,
@@ -136,7 +138,18 @@ class CacheableLoader {
       try {
         final cachedData = await cacheManager.get<T>(key);
         if (cachedData != null || cacheOnly) {
-          // 缓存中有数据，返回缓存数据
+          // 如果需要在返回缓存后刷新，且不是仅缓存模式
+          if (refreshAfterCache && !cacheOnly) {
+            // 在后台执行加载和缓存更新
+            _refreshCache(
+              key,
+              loader: loader,
+              cacheManager: cacheManager,
+              loaderValidator: loaderValidator,
+              flushWhenSuccess: flushWhenSuccess,
+            );
+          }
+          // 返回缓存数据
           return CacheableLoadResult.fromCache(cachedData);
         }
       } catch (e) {
@@ -160,6 +173,28 @@ class CacheableLoader {
     } catch (e) {
       // 加载器加载失败，返回错误信息
       return CacheableLoadResult.failed(e);
+    }
+  }
+
+  // 新增一个私有方法用于后台刷新缓存
+  static Future<void> _refreshCache<T>(
+    String key, {
+    required Future<T?> Function() loader,
+    required SmartCacheManager cacheManager,
+    bool Function(T? data)? loaderValidator,
+    bool flushWhenSuccess = false,
+  }) async {
+    try {
+      final loadedData = await loader();
+      bool willCache = loadedData != null;
+      if (loaderValidator != null) {
+        willCache = loaderValidator(loadedData);
+      }
+      if (willCache && loadedData != null) {
+        cacheManager.put<T>(key, loadedData, flushNow: flushWhenSuccess);
+      }
+    } catch (e) {
+      debugPrint('后台刷新缓存失败: $e');
     }
   }
 }
